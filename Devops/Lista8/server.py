7ae11b5ffa6dab7602e3e9b17b51a5dbe7e7ea1f
Michal Wilczkowski <michik1324@gmail.com>
2025-12-03 14:17:15 +0100

    devops lista 8

--- Devops/Lista8/server.py
+++ Devops/Lista8/server.py
@@ -1,4 +1,4 @@
-import sys
+import os
 import uvicorn
 from fastapi import FastAPI
 from random import randint
@@ -9,11 +9,9 @@ app = FastAPI()
 async def listen():
     if randint(0, 101) > 50:
         print("Wylaczam aplikacje")
-        # sys.exit(1) kończy proces Pythona z kodem błędu 1 (oznaczającym błąd)
-        sys.exit(1)
+        os._exit(1)
     
     return {"status": "ACK", "message": "Wszystko dziala poprawnie"}
 
 if __name__ == "__main__":
-    # Uruchomienie serwera bezpośrednio z pliku
     uvicorn.run(app, host="0.0.0.0", port=8000)
